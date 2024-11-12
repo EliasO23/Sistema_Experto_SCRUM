717a26090b33fae7f6b73ee25b9e46d78f89cfb7
EliasO23 <eliasoliva08@gmail.com>
2024-11-11 23:11:25 -0600

    comments

--- static/javascript/alertas.js
+++ static/javascript/alertas.js
@@ -1,14 +1,14 @@
-// Función para iniciar el motor de inferencia mediante una solicitud HTTP
-function iniciarMotor() {
-    fetch('/iniciar_motor')
-    .then(response => response.json())
-    .then(data => {
-        var recomendacionesDiv = document.getElementById('recomendaciones');
-        recomendacionesDiv.innerHTML = '';  // Limpiamos recomendaciones previas
-        data.forEach(function(mensaje) {
-            var nuevaRecomendacion = document.createElement('p');
-            nuevaRecomendacion.innerHTML = mensaje;
-            recomendacionesDiv.appendChild(nuevaRecomendacion);
-        });
-    });
-}
+// // Función para iniciar el motor de inferencia mediante una solicitud HTTP
+// function iniciarMotor() {
+//     fetch('/iniciar_motor')
+//     .then(response => response.json())
+//     .then(data => {
+//         var recomendacionesDiv = document.getElementById('recomendaciones');
+//         recomendacionesDiv.innerHTML = '';  // Limpiamos recomendaciones previas
+//         data.forEach(function(mensaje) {
+//             var nuevaRecomendacion = document.createElement('p');
+//             nuevaRecomendacion.innerHTML = mensaje;
+//             recomendacionesDiv.appendChild(nuevaRecomendacion);
+//         });
+//     });
+// }
